34d0b4cfdd951f349b948c8e5ac68ab2de1e7b0d
Andrey Ravkov <andreyravkov@gmail.com>
2026-04-18 20:16:33 +0200

    add new list extensions

--- src/NetExt.Utils/Collections/CollectionsExtensions.cs
+++ src/NetExt.Utils/Collections/CollectionsExtensions.cs
@@ -2,18 +2,44 @@ namespace NetExt.Utils.Collections;
 
 public static class CollectionsExtensions
 {
+    /// <summary>
+    /// Convert element to list
+    /// </summary>
+    /// <param name="source">Source element to convert</param>
+    /// <typeparam name="T">The type of the element in the list.</typeparam>
+    /// <returns></returns>
     public static IList<T> ToListExt<T>(this T? source)
     {
         return source is null ? Array.Empty<T>() : new List<T> { source };
     }
-    
-    public static void ForEachExt<T>(this IEnumerable<T> list, Action<T> action)
-    {
-        foreach (var item in list)
+
+    /// <summary>
+    /// Convert IEnumerable to IReadOnlyList without duplication
+    /// </summary>
+    /// <param name="source"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IReadOnlyList<T> AsReadOnlyListExt<T>(this IEnumerable<T>? source) =>
+        source switch
         {
-            action(item);
+            null => [],
+            IReadOnlyList<T> roList => roList,
+            IList<T> list => list as IReadOnlyList<T>,
+            _ => source.ToList(),
         }
-    }
+        ?? [];
     
-    
+    /// <summary>
+    /// Convert IEnumerable to List without duplication
+    /// </summary>
+    /// <param name="source"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static List<T> AsListExt<T>(this IEnumerable<T>? source) =>
+        source switch
+        {
+            null => [],
+            List<T> list => list,
+            _ => source.ToList(),
+        };
 }
